72833c1d70c23de73872609ed0ef4b7df73e9edc
Azurhys <123641752+Azurhys@users.noreply.github.com>
2024-08-13 22:35:24 +0200

    Navbar init !

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,7 +1,32 @@
+import React from 'react';
+
 const Navbar = () => {
-    return ( <>
-        NAVBAR
-    </> );
+  return (
+    <nav className="navbar navbar-expand-lg" style={{ background: 'linear-gradient(to right, #FF0000, #0000FF)' }}>
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#home">MyWebsite</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#services">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
- 
+
 export default Navbar;
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './app.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
